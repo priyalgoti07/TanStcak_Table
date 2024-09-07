ccf69143113fe47816f1601e256259d728626a2f
priyal.finestar <priyal@finestar.in>
2024-09-07 16:38:19 +0530

    sorting add usertable

--- src/components/Tableback.tsx
+++ src/components/Tableback.tsx
@@ -1,6 +1,7 @@
 import '../App.css';
-import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { ColumnDef, flexRender, getCoreRowModel, getSortedRowModel, SortingState, useReactTable } from '@tanstack/react-table';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TableSortLabel } from '@mui/material';
+import { useState } from 'react';
 
 interface TableProps<TData> {
     data: TData[],
@@ -8,11 +9,18 @@ interface TableProps<TData> {
 }
 
 const Tableback = <TData,>({ data, columns }: TableProps<TData>) => {
+    // Add sorting state to track sorting configuration
+    const [sorting, setSorting] = useState<SortingState>([])
 
     const table = useReactTable({
         data,
         columns,
         getCoreRowModel: getCoreRowModel(),
+        state: {
+            sorting, //manage sorting here
+        },
+        onSortingChange: setSorting,
+        getSortedRowModel: getSortedRowModel() // This enables to sorting
     })
 
     console.log("setData", table);
@@ -26,10 +34,19 @@ const Tableback = <TData,>({ data, columns }: TableProps<TData>) => {
                             <TableRow key={headerGroup.id}>
                                 {
                                     headerGroup.headers.map((header) => (
-                                        <TableCell key={headerGroup.id} sx={{ backgroundColor: "#6b7280", fontWeight: "600" }}>
-                                            {header.isPlaceholder
-                                                ? null
-                                                : flexRender(header.column.columnDef.header, header.getContext())}
+                                        <TableCell key={header.id} sx={{ backgroundColor: "#6b7280", fontWeight: "600" }}>
+                                            {header.isPlaceholder ? null : (
+                                                <TableSortLabel
+                                                    active={header.column.getCanSort()} // Check if column is sortable
+                                                    direction={header.column.getIsSorted() === 'desc' ? 'desc' : 'asc'}
+                                                    onClick={header.column.getToggleSortingHandler()} // Toggle sorting when clicked
+                                                >
+                                                    {flexRender(
+                                                        header.column.columnDef.header,
+                                                        header.getContext()
+                                                    )}
+                                                </TableSortLabel>
+                                            )}
                                         </TableCell>
                                     ))
                                 }
